f76ddd16c59190581d0a8558043b895f0ea2fd6b
eden laboda <eden.laboda@gmail.com>
2016-03-20 21:16:51 +0100

    creating the models, controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "french",
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "french",
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "french",
+  },
+  {
+    name:         "La Seine",
+    address:      "Paris",
+    category:     "french",
+  },
+  {
+    name:         "Tamise",
+    address:      "London",
+    category:     "french",
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
+
